dce9df180c57c3ef48800aee55edf682cd1906ad
Lucas Churchill <lucas@agtlucas.com>
2014-10-11 10:46:57 -0300

    Update blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,5 +3,18 @@ class BlogsController < ApplicationController
   end
 
   def create
+    blog = Blog.new(blog_params)
+    if blog.save
+      flash[:notice] = 'Blog successfully created.'
+      render json: {:message => flash[:notice]}
+    else
+      flash[:error] = 'Error'
+      render json: flash
+    end
   end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:name)
+    end
 end
