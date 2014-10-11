780aae2a5cd4554e45af8f3c460e3b829188e6a6
Lucas Churchill <lucas@agtlucas.com>
2014-10-10 23:34:29 -0300

    Adding Blogs controller

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,7 @@
+class BlogsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'blogs/new'
+
+  get 'blogs/create'
+
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
