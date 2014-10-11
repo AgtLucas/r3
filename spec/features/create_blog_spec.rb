fda9bc5243f415b83cdbd6716e27bc4d2fe52479
Lucas Churchill <lucas@agtlucas.com>
2014-10-11 10:32:39 -0300

    Update create_blog_spec

---
+++ spec/features/create_blog_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'Creating blog' do
+  scenario 'Can create a blog', :js => true do
+    visit '/'
+    click_link 'New Blog'
+
+    fill_in 'Name', with: 'My blog'
+    click_button 'Create Blog'
+    expect(page).to have_content('Blog successfully created.')
+  end
+end
